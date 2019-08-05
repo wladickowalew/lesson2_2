6a1bd357696bffad013c943e00184a3c0240da51
Wlad Kowalew <wladickowalew@gmail.com>
2019-08-05 12:31:45 +0300

    equalls dictionary

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("begin");
+
+//сравнение словарей
+let d = {key: "value"};
+let a = {key: "value"};
+if (a==d){
+	console.log("dict Yes");
+}else{
+	console.log("dict No");
+}
+a = d;
+if (a==d){
+	console.log("dict Yes");
+}else{
+	console.log("dict No");
+}
